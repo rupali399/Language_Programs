2b38509052b7787838c64a1270b3382080bc4891
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-07-12 21:32:43 +0530

    Add files via upload

---
+++ C++ PROGRAMS/basicio.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter the value of num1"<<endl;
+    cin>>num1;
+
+    cout<<"enter the value of num2"<<endl;
+    cin>>num2;
+
+    cout<<"the sum is :"<<num1+num2;
+    return 0;
+}   
+
