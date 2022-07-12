9ead3435d3f4d04372a78e2bf0e3867332f09b4a
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-07-12 22:03:19 +0530

    Add files via upload

---
+++ C++ PROGRAMS/Typecast.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=45;
+    float b=45.46;
+    cout<<"The value of a is:"<<(float)a<<endl;
+    cout<<"The value of a is:"<<float(a)<<endl;
+    cout<<"The value of a is:"<<(int)a<<endl;
+    cout<<"The value of a is:"<<int(a)<<endl;
+    return 0;
+}
