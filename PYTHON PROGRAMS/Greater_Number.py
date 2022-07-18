14fa091afd87902b43978564cdc79c6da6d80e30
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-07-18 21:53:19 +0530

    Add files via upload

---
+++ PYTHON PROGRAMS/Greater_Number.py
@@ -0,0 +1,10 @@
+a = int(input('Enter 1st number : '))
+b = int(input('Enter 2nd number : '))
+if a > b:
+    print(a, 'is greater number')
+    
+elif a == b:
+    print(a, 'is equal to', b)
+        
+else:
+    print(b, 'is greater number')
