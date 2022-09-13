58f4b7f38924aa9b878fb4d8dade6a302d368990
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-13 19:39:25 +0530

    Add files via upload

---
+++ PYTHON/Boolean_Logic.py
@@ -0,0 +1,23 @@
+#name = "admin"
+#password = "admin123"
+
+#if name == "admin" and password == "admin123":
+#    print("valid user")
+#else:
+#    print("invalid user")
+
+#name = "admin"
+#password = "admin1"
+
+#if name == "admin" and password == "admin123":
+#    print("valid user")
+#else:
+#    print("invalid user")
+
+name = "user"
+password = "admin123"
+
+if name == "admin" or password == "admin123":
+    print("valid user")
+else:
+    print("invalid user")
