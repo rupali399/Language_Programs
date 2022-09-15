061bba583f852dffeeaca3cbf9878f66875957ec
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-15 15:55:34 +0530

    Add files via upload

---
+++ PYTHON/For_Loop_in_Python.py
@@ -0,0 +1,12 @@
+#for x in range(1, 11):
+#    print(x)
+    
+fruits = ["Mango", "Banana", "Apple", "Peach"]
+
+#for x in fruits:
+#    print(x)
+
+# Even Numbers
+
+for x in range(0, 20, 2):
+    print(x)
