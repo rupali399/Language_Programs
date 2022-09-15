4117510f7ebc1f030d6cb00a85f46bae8e168e1c
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-15 15:59:08 +0530

    Add files via upload

---
+++ PYTHON/List_Operations.py
@@ -0,0 +1,13 @@
+numbers = [1, 1, 1, 1, 1]
+newnumbers = [2, 2, 2, 2, 2]
+
+#numbers[2] = 5
+
+#print(numbers + newnumbers)
+
+#print(numbers * 3)
+
+fruits = ["Mango", "Guava", "Peach"]
+
+#print("Mango" in fruits)
+print("Banana" in fruits)
