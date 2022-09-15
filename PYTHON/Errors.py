fcd5da7dce059f205993fce16ba76fefc7718c61
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-15 15:53:42 +0530

    Add files via upload

---
+++ PYTHON/Errors.py
@@ -0,0 +1,22 @@
+# Syntex Error
+
+#def function1(a, b)
+#    print(a + b)
+    
+#function(2, 3)
+
+#Logical Error
+
+#def function1(a, b):
+#    print(a * b)
+    
+#function1(2, 3)
+
+#DevideByZero Error
+
+#def function1(a, b):
+#    print(a / b)
+    
+#function1(20, 0)
+#print("hello")
+
