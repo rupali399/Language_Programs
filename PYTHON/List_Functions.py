8e477c79a8245b9dc1af5fd90da335769d327c31
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-15 15:57:57 +0530

    Add files via upload

---
+++ PYTHON/List_Functions.py
@@ -0,0 +1,11 @@
+fruits = ["Mango", "Apple", "Peach"]
+
+#fruits.append("Banana")
+#print(fruits)
+
+#print(len(fruits))
+
+#fruits.insert(1, "Guava")
+#print(fruits)
+
+print(fruits.index("Apple"))
