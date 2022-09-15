f732dd1769ef412e3c5cc100db2ff489fbb9dee6
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-15 16:01:03 +0530

    Add files via upload

---
+++ PYTHON/Simple_Intterest_Calculator.py
@@ -0,0 +1,11 @@
+p = input('Enter a value of p : ')
+n = input('Enter a value of n : ')
+r = input('Enter a value of r : ')
+
+p = int(p)
+n = int(n)
+r = int(r)
+
+si = (p * n * r) / 100
+
+print(si)
