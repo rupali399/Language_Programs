894f8a50ffb2d1f0841e20ea87bc4631e8e3ab84
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-16 15:49:15 +0530

    Add files via upload

---
+++ PYTHON/String_Formatting.py
@@ -0,0 +1,10 @@
+#numbers = [4, 5, 6]
+#newstring = "Numbers : [0][1][2]".format(numbers[0], numbers[1], numbers[2])
+#print(newstring)
+
+numbers = [4, 5, 6]
+newstring = "Numbers : [0], [1], [2]".format(numbers[0], numbers[1], numbers[2])
+print(newstring)
+
+a = "[x] / [y]".format(x = 100, y = 200)
+print(a) 
