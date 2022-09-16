0c4f2d056224b9a4ffb6ee8bb22f10b2d9885895
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-16 15:48:51 +0530

    Add files via upload

---
+++ PYTHON/String_Functions.py
@@ -0,0 +1,15 @@
+#print(",".join(["Apple", "Mango", "Peach"]))
+
+#print("Hello There".replace("There", "World"))
+
+#newstirng = "Hello There"
+#print(newstirng.replace("There", "World"))
+
+newstring = "This is a string"
+print(newstring.startswith("This"))
+
+print(newstring.endswith("string"))
+
+print(newstring.upper())
+
+print(newstring.lower())
