37c7f3d5dbcfb9c373d59816167a70be084f33bd
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-23 15:50:50 +0530

    Add files via upload

---
+++ PYTHON/Sets.py
@@ -0,0 +1,21 @@
+#numbers = {1, 2, 3, 4, 5, 6, 7, 8}
+#print(numbers)
+
+#print(5 in numbers)
+
+#print(9 in numbers)
+
+#numbers.add(9)
+#print(numbers)
+
+#numbers.remove(5)
+#print(num)
+
+seta = {1, 2, 3, 4, 5, 6}
+setb = {4, 5, 6, 7, 8, 9}
+
+print(seta | setb)
+
+print(seta & setb)
+
+print(seta - setb)
