2bbddf08d3692dbe9422015d8bc85256606bd622
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-23 15:53:03 +0530

    Add files via upload

---
+++ PYTHON/Generators.py
@@ -0,0 +1,15 @@
+#def function():
+#    counter = 0
+#    while counter < 5:
+#        yield counter
+#        counter += 1
+        
+#for x in function():
+#        print(x)
+
+def even_number(x):
+    for i in range(x) :
+        if i % 2 == 0:
+            yield i
+            
+print(list(even_number(20)))
