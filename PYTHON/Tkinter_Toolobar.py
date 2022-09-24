6c9c07ed469f42ca36910dafe8f4ab6dc09a2cae
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-24 09:16:39 +0530

    Add files via upload

---
+++ PYTHON/Tkinter_Toolobar.py
@@ -0,0 +1,39 @@
+from tkinter import * 
+
+def function1():
+    print("Menu  Button Clicked")
+    
+root = Tk()
+
+mymenu = Menu(root)
+root.config(menu = mymenu)
+
+submenu = Menu(mymenu)
+
+mymenu.add_cascade(label = "File", menu = submenu)
+submenu.add_command(label = "Project", command = function1)
+submenu.add_command(label = "Save", command = function1)
+
+submenu.add_separator()
+submenu.add_command(label = "Exit", command = function1)
+
+newmenu = Menu(mymenu)
+mymenu.add_cascade(label = "Edit", menu = newmenu)
+newmenu.add_command(label = "Undo", command = function1)
+
+thismenu = Menu(mymenu)
+mymenu.add_cascade(label = "About", menu = thismenu)
+thismenu.add_command(label = "Contact", command = function1)
+
+# Toolbar
+
+toolbar = Frame(root, bg = "Pink")
+insertbutton = Button(toolbar, text = "insert files", command = function1)
+insertbutton.pack(side = LEFT, padx = 2, pady = 3)
+
+printbutton = Button(toolbar, text = "print", command = function1)
+printbutton.pack(side = LEFT, padx = 2, pady = 3)
+
+toolbar.pack(side = TOP, fill = X)
+
+root.mainloop()
