e1568cb2d1d78ee5f18e0621c93e95d004e3fa84
RUPALI SAHU <90509269+rupali399@users.noreply.github.com>
2022-09-24 09:15:44 +0530

    Add files via upload

---
+++ PYTHON/Tkinter_Making_Stattus_Bar.py
@@ -0,0 +1,42 @@
+from tkinter import * 
+
+def function1():
+    print("Menu item clicked")
+
+root = Tk()
+
+mymenu = Menu(root)
+root.config(menu = mymenu)
+
+submenu = Menu(mymenu)
+
+mymenu.add_cascade(label = "File", menu = submenu)
+
+submenu.add_command(label = "Project", command = function1)
+submenu.add_command(label = "Save", command = function1)
+
+submenu.add_separator()
+submenu.add_command(label = "Exit", command = function1)
+
+newmenu = Menu(mymenu)
+mymenu.add_cascade(label = "Edit", menu = newmenu)
+
+newmenu.add_command(label = "Undo", command = function1)
+
+# Toolbar
+
+toolbar = Frame(root, bg = "Pink")
+insertbutton = Button(toolbar, text = "insert files", command = function1)
+insertbutton.pack(side = LEFT, padx = 2, pady = 3)
+
+printbutton = Button(toolbar, text = "Print", command = function1)
+printbutton.pack(side = LEFT, padx = 2, pady = 3)
+
+toolbar.pack(side = TOP, fill = X)
+
+# Status Bar
+
+status = Label(root, text = "This is the status", bd = 2, relief = SUNKEN, anchor = W)
+status.pack(side = BOTTOM, fill = X)
+
+root.mainloop()
